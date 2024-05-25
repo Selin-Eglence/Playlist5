c38e459e961aac1e94ecf5ea18d7b3d3b82c3c33
Selin-Eglence <sel.eglence@yandex.ru>
2024-05-25 20:03:49 +0300

    спринт 11

--- app/src/main/java/com/practicum/playlist5/Track.kt
+++ app/src/main/java/com/practicum/playlist5/Track.kt
@@ -3,5 +3,5 @@ package com.practicum.playlist5
 data class Track(
     val trackName: String,
     val artistName: String,
-    val trackTime: String,
+    val trackTimeMillis: Long,
     val artworkUrl100: String)
