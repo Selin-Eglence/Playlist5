ea3deabad17f24147b14bdcd2913552f1acbbe07
Selin-Eglence <sel.eglence@yandex.ru>
2024-11-19 00:35:54 +0300

    20 спринт

--- app/src/main/java/com/practicum/playlist5/search/domain/models/SearchResult.kt
+++
@@ -1,6 +0,0 @@
-package com.practicum.playlist5.search.domain.models
-
-sealed class SearchResult {
-    data class Success(val tracks: List<Track>?, val expression: String) : SearchResult()
-    data class Error(val message: String?) : SearchResult()
-}
